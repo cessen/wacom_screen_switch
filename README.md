7eef126823e11169a5e232d5a1f1fbf871eddd41
Nathan Vegdahl <cessen@cessen.com>
2012-10-06 08:21:38 -0700

    First commit.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 wacom_screen_switch
 ===================
 
-A small script that cycles your wacom tablet between screens on a multi-monitor setup
+A small script that cycles your wacom tablet between screens on a multi-monitor setup.
+
+This script is Linux-specific and requires both xsetwacom and xrandr to be installed.
+
+The first time the script is run, it starts a long-running process, and sets
+the wacom tablet to map to the first monitor.  Subsequent runs simply signal
+the long-running process to cycle to the next monitor.
+
+The intent of the script is to be hooked up to a hotkey, via which the user
+can then quickly cycle between monitors.  It is also best if the script is run
+once on login as one of the user's auto-started programs, so that the
+long-running process is already started and the wacom tablet already mapped.
---
+++ wacom_screen_switch.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+""" A simple script that cycles your wacom device through the various screens
+    in a multi-monitor setup.  It is meant to be hooked up to a hot key, for
+    example, to allow the user to quickly navigate between screens.
+
+    It requires both xsetwacom and xrandr to be installed.
+"""
+
+import os
+import os.path
+import time
+import signal
+import subprocess
+import tempfile
+
+
+# The signal to use for communication between processes
+mysig = signal.SIGRTMIN + 1
+if mysig > signal.SIGRTMAX:
+	exit(1)
+
+pid = str(os.getpid())
+pidfile = tempfile.gettempdir() + "/" + "tmp_wacom_screen_switch_pid.pid"
+screen_device_names = []
+wacom_device_names = []
+screen_device_index = -1
+
+
+def get_wacom_device_names():
+    """ Returns a list of wacom device names from xsetwacom.
+    """
+    proc = subprocess.Popen(["xsetwacom", "--list", "devices"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    proc.wait()
+    lines = out.split("\n")
+
+    devices = []
+    for line in lines:
+        if "id:" in line:
+            dev, tail = line.split("id:", 1)
+            devices += [dev.strip()]
+    return devices
+
+
+def get_screen_device_names():
+    """ Returns a list of connected screen device names from xrandr.
+    """
+    proc = subprocess.Popen("xrandr", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    proc.wait()
+    lines = str(out).split("\n")
+
+    devices = []
+    for line in lines:
+        if "connected" in line and "disconnected" not in line:
+            dev, tail = line.split(" ", 1)
+            devices += [dev.strip()]
+    return devices
+
+
+def cycle_screen(sig, stack):
+    # Ignore further signals while working
+    signal.signal(mysig, signal.SIG_IGN)
+    
+    global screen_device_index
+    screen_device_index = (screen_device_index + 1) % len(screen_device_names)
+    
+    sdev = screen_device_names[screen_device_index]
+    for dev in wacom_device_names:
+        subprocess.Popen(["xsetwacom", "--set", dev, "MapToOutput", sdev]).wait()
+    
+    # Watch for signals again
+    signal.signal(mysig, cycle_screen)
+
+
+def cleanup_and_exit(sig, stack):
+    os.remove(pidfile)
+    exit(0)
+
+
+def main_loop():
+    global screen_device_names
+    global wacom_device_names
+    global screen_device_index
+
+    screen_device_names = get_screen_device_names()
+    wacom_device_names = get_wacom_device_names()
+    screen_device_index = -1
+    cycle_screen(None, None)
+
+    # Set up screen switch signal
+    signal.signal(mysig, cycle_screen)
+    with open(pidfile, "w") as f:
+        f.write(pid)
+        f.flush()
+    
+    # Set up exit signals
+    signal.signal(signal.SIGHUP, cleanup_and_exit)
+    signal.signal(signal.SIGQUIT, cleanup_and_exit)
+    signal.signal(signal.SIGABRT, cleanup_and_exit)
+    signal.signal(signal.SIGTERM, cleanup_and_exit)
+
+    # Sit and wait for signals
+    while True:
+        time.sleep(60) # Arbitrary number of seconds - will be interrupted by caught signal
+        
+        # Check to make sure the pidfile still exists
+        # and still represents this process.  This ensures
+        # that this process doesn't keep running indefinitely
+        # even after it become useless.
+        try:
+            f = open(pidfile, "r")
+        except:
+            exit(1)
+        finally:
+            fpid = f.readline().strip()
+            if fpid != pid:
+                exit(1)
+
+
+if __name__ == "__main__":
+    if os.path.exists(pidfile):
+        # If the main process already appears to be running,
+        # send it a signal to switch screens.
+        with open(pidfile, "r") as f:
+            fpid = int(f.readline().strip())
+        try:
+            os.kill(fpid, mysig)
+            print "Already started!  Switch signal sent."
+        except OSError:
+            # If it turns out the main process isn't actually running,
+            # start this as the main process
+            main_loop()
+    else:
+        main_loop()
+
+
